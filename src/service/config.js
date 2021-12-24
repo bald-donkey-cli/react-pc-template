a2afdf195d472e948301ee814681492be1ae8ad6
贾赛 <jiasai@aegis-data.cn>
2021-12-24 14:46:58 +0800

    配置环境变量，分辨不同环境

--- src/service/config.js
+++ src/service/config.js
@@ -1,7 +1,23 @@
-const devBaseURL = '';
-const proBaseURL = '';
+const devBaseURL = 'http://chst.vip:8081/'; // 测试环境后端地址
+const proBaseURL = 'http://chst.vip:8081/'; // 正式环境后端地址
 
-export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL : proBaseURL;
-// console.log("---+++", process.env.NODE_ENV);
+const BASE_URL = process.env.REACT_APP_BUILD_ENV === 'development' ? devBaseURL : proBaseURL;
+console.log("---", process.env.REACT_APP_BUILD_ENV);
+console.log("---+", BASE_URL);
 
-export const TIMEOUT = 10000; // 配置请求时长
+const TIMEOUT = 10000; // 配置请求时长
+const TimeoutNum = 3; // 超时尝试次数
+const IntervalTime = 1000; // 超时重新请求间隔
+const SuccessCode = [200, '200']; // 操作正常code，支持String、Array、int多种类型
+const StatusName = 'code'; // 数据状态的字段名称
+const MessageName = 'msg'; // 状态信息的字段名称
+
+module.exports = {
+  BASE_URL,
+  TIMEOUT,
+  TimeoutNum,
+  IntervalTime,
+  SuccessCode,
+  StatusName,
+  MessageName
+}
